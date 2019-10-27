186b589c305b90bd73da82d02c215b710e4be766
ezequielzacca <ezequielzacca@gmail.com>
2019-10-26 23:19:43 -0300

    simple economy modified to drop loot on mob death

---
+++ mod/simple-economy-modified/scripts/SEscripts/strings_es.lua
@@ -0,0 +1,43 @@
+STRINGS.NAMES.DEER_ANTLER3 = "Deer Antler"
+STRINGS.NAMES.SUCCULENT_PICKED = STRINGS.NAMES.SUCCULENT_PICKED or "Succulent"
+STRINGS.NAMES.WINTER_ORNAMENT_LIGHT1 = STRINGS.NAMES.WINTER_ORNAMENT_LIGHT1 or "Red Christmas Orb"
+STRINGS.NAMES.WINTER_ORNAMENT_LIGHT2 = STRINGS.NAMES.WINTER_ORNAMENT_LIGHT2 or "Green Christmas Orb"
+STRINGS.NAMES.WINTER_ORNAMENT_LIGHT3 = STRINGS.NAMES.WINTER_ORNAMENT_LIGHT3 or "Blue Christmas Orb"
+STRINGS.NAMES.WINTER_ORNAMENT_LIGHT4 = STRINGS.NAMES.WINTER_ORNAMENT_LIGHT4 or "White Christmas Orb"
+
+STRINGS.NAMES.SECOIN = "Simple Oro"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.LUCKAMULET = "Simple Oro"
+
+STRINGS.NAMES.STEALER = "Roba Oro"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.STEALER = "Proveedor de riqueza multiproposito"
+
+STRINGS.NAMES.GOLDSTAFF = "Mano de Midas"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.GOLDSTAFF = "Convierte todo en oro"
+
+STRINGS.NAMES.VIPCARD = "Tarjeta VIP"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.VIPCARD = "Para los clientes favoritos"
+
+STRINGS.NAMES.LUCKAMULET = "Amuleto de la Suerte"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.LUCKAMULET = "Riqueza en una gema"
+
+STRINGS.SIMPLEECONOMY={
+[1] = "Empecemos el camino a la riqueza",
+[2] = "Comprar",
+[3] = "Instrucciones",
+[4] = "Atras",
+[5] = "Cerrar",
+[6] = "Siguiente",
+[7] = "Anterior",
+[8] = "Bluprint al azar",
+[9] = "+ ",
+[10] = "+ ",
+[11] = "Estos items no tienen precio.",
+[12] = "No hay nada para desintegrar.",
+[13] = "Mi nivel de comprador aumento!",
+[14] = "Psst... Yo tengo ",
+[15] = "Oro.",
+[16] = "No te sorprendas, tengo ",
+[17] = "Oro ahorrado.",
+[18] = " Oro",
+[19] = " Oro en total",
+}
